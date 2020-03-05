d3ae76b01a0e6cd42208b2b1dc63146a08e0bb26
Rohini Senthil <rohinivsenthil@gmail.com>
2020-03-05 21:08:42 +0530

    feat[routes]: add route for getCart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { getProducts } = require('../handlers/getProducts');
 const { updateCart } = require('../handlers/updateCart');
+const { getCart } = require('../handlers/getCart');
 
 const routeArray = [
   { path: '/products', method: 'GET', handler: getProducts },
   { path: '/items', method: 'POST', handler: updateCart },
+  { path: '/items', method: 'GET', handler: getCart },
 ];
 module.exports = routeArray;
